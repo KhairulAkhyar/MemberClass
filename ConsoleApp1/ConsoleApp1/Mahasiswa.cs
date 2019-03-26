574c01405e2453e1b894b71446eec6c5a0a99e27
Akhyar <khairulakhyar02@gmail.com>
2019-03-26 11:52:39 +0700

    Console

---
+++ ConsoleApp1/ConsoleApp1/Mahasiswa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama: {0}", Nama);
+            Console.WriteLine("NIM :{0}", Nim);
+            Console.WriteLine("IPK :{0}", Ipk);
+            Console.WriteLine("Telah Melakukan Registrasi");
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} Sedang Melakukan Proses Krs\n", Nama);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+            saya.Nim = "123456";
+            saya.Nama = "Jono";
+            saya.Ipk = 3.95f;
+
+            kamu.Nim = "54321";
+            kamu.Nama = "Joni";
+            kamu.Ipk = 3.70f;
+
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+            Console.ReadKey();
+        }
+    }
+}

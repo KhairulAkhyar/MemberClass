970736273e70ec8fffeb253e04037d99d5e99443
Akhyar <khairulakhyar02@gmail.com>
2019-03-26 11:48:22 +0700

    MemberClass

---
+++ LatihanMemberClass/LatihanMemberClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi=new Taxi();
+
+            taxi.DriverName = "Akyar";
+            taxi.OnDuty=false;
+            taxi.NumPassenger=10;
+
+            taxi.taxiinfo();
+            taxi.antar();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LatihanMemberClass/LatihanMemberClass/Taxi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+        
+        public void taxiinfo(){
+            Console.WriteLine("Nama          : {0}", DriverName);
+            Console.WriteLine("OnDuty        : {0}", OnDuty ? "Yes" : "No");
+            Console.WriteLine("NumPassenger  : {0}", NumPassenger);
+            Console.WriteLine("Mengambil Penumpang");
+}
+        public void antar(){
+            Console.WriteLine("Sedang Antar Penumpang");
+
+}
+
+    }
+}

11b646cf9cc0a7d4668784a281f2e3b219c9f554
Khairul Akhyar <khairulakhyar02@gmail.com>
2019-04-09 12:18:47 +0700

    encaplusation

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time objTime = new Time(2019, 04, 09, 12, 10, 34);
+            Console.WriteLine("Year   : {0}", objTime.Year.ToString());
+            Console.WriteLine("Month  : {0}", objTime.Month.ToString());
+            Console.WriteLine("Date   : {0}", objTime.Date);
+            Console.WriteLine("Hour   : {0}", objTime.Hour);
+            Console.WriteLine("Minute : {0}", objTime.Minute);
+            Console.WriteLine("Second : {0}", objTime.Second);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Time.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Time
+    {
+        // private variables atau field
+        public int year;
+        private int month;
+        private int date;
+        private int hour;
+        private int minute;
+        private int second;
+
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Date { get; set; }
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+            public Time(int year, int month, int date, int hour, int minute, int second)
+        {
+            this.Year = year;
+            this.Month = month;
+            this.Date = date;
+            this.Hour = hour;
+            this.Minute = minute;
+            this.Second = second;
+        }
+        public void DisplayCurrentTime()
+        {
+            Console.WriteLine("Waktu sekarang : {0}", DateTime.Now.ToString());
+        }
+    }
+}
